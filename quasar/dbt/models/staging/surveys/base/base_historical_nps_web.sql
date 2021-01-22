97413b9f2f75d70129229abf6a7bbaf7ae2acc30
ghangas-ds <pghangas@dosomething.org>
2021-01-22 16:25:41 -0500

    updated model structure based on feedback

--- quasar/dbt/models/staging/surveys/base/base_historical_nps_web.sql
+++ quasar/dbt/models/staging/surveys/base/base_historical_nps_web.sql
@@ -2,6 +2,10 @@
 SELECT
 id::varchar(256) as survey_response_id
 , nps_score::int
+, (case when nps_score::int <= 6 then 'detractor'
+     when nps_score::int <= 8 then 'passive'
+     when nps_score::int <= 10 then 'promoter'
+     else 'invalid score' end) as net_promoter_cat
 , nps_reason::text
 , northstar_id::varchar(256)
 , url as surveyed_on_url
